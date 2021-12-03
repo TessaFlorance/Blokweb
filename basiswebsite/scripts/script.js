97a269bb4bef5a3f36466ba252d0331cff169937
TessaFlorance <94309218+TessaFlorance@users.noreply.github.com>
2021-12-03 11:53:24 +0100

    Add files via upload

---
+++ basiswebsite/scripts/script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toonMenu); 
+
+function toonMenu() {
+    var deNav = document.querySelector("nav");
+    deNav.classList.toggle("menuOpen"); 
+}
